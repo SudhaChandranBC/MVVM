aa5b4f560050caa63becb45e1268e11e961e437c
Chandran, Sudha <sudhachandran1bc@gmail.com>
2019-06-26 15:02:40 +0530

    Initial Commit

---
+++ MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by Chandran, Sudha | SDTD on 26/06/19.
+//  Copyright © 2019 Chandran, Sudha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
